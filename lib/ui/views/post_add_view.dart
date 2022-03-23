1dae23106769d2eb15cd5b0569f940c151d9ff91
Chukwudi Wisdom <55758868+override-bot@users.noreply.github.com>
2022-03-23 08:17:25 +0100

    clean up

--- lib/ui/views/post_add_view.dart
+++ lib/ui/views/post_add_view.dart
@@ -7,64 +7,85 @@ import 'package:spacepal/core/viewmodels/post_view_model.dart';
 import 'package:spacepal/core/viewmodels/user_view_model.dart';
 import 'package:spacepal/utils/router.dart';
 
-class PostAddView extends StatefulWidget{
+class PostAddView extends StatefulWidget {
   final bool isImportant;
-  PostAddView({
-    required this.isImportant
-  });
+  PostAddView({required this.isImportant});
   @override
-  PostAddViewState createState()=> PostAddViewState();
+  PostAddViewState createState() => PostAddViewState();
 }
-class PostAddViewState extends State<PostAddView>{
+
+class PostAddViewState extends State<PostAddView> {
   RouteController router = RouteController();
   TextEditingController _postField = TextEditingController();
   final User? user = FirebaseAuth.instance.currentUser;
+  var isPost = false;
   @override
   Widget build(BuildContext context) {
-     final userProvider = Provider.of<UserViewModel>(context);
-     final postProvider = Provider.of<PostViewModel>(context);
-   return Scaffold(
-     appBar: AppBar(
-       backgroundColor:Colors.white,
-       elevation:0.0,
-     leading:IconButton(icon:Icon(Icons.arrow_back, color:Colors.blue[600]), onPressed:(){
-       router.pop(context);
-     },
-    
-     ),
-     actions:[
-       IconButton(onPressed: ()async{
-      var snapshot = await   userProvider.getStaffById(user!.uid);
-         postProvider.addPosts(Posts(post: _postField.text, posterId: user!.uid, username: snapshot.username,
-         imageUrl: snapshot.imageUrl, timeStamp: DateTime.now(), isImportant: widget.isImportant)).then((value) {
-           router.pop(context);
-         });
-       }, icon: Icon(Icons.post_add, color:Colors.blue[600]))
-     ],
-     title:Text("Add General Post",
-     style: TextStyle(
-       color:Colors.blue[600],
-       fontSize: 20,
-       fontWeight:FontWeight.w500
-     ))
-     ),
-     body:Container(
-       height:MediaQuery.of(context).size.height,
-       padding: EdgeInsets.all(20),
-       color: Colors.white,
-       width: MediaQuery.of(context).size.width,
-       child: TextFormField(
-controller:_postField,
-minLines:100,
-maxLines: 250,
-maxLength:750,
-autofillHints:const ["How are we doing today","whats going on at the office today"],
-decoration: InputDecoration(
-  hintText: "What is going on?"
-),
-       ),
-     )
-   );
+    final userProvider = Provider.of<UserViewModel>(context);
+    final postProvider = Provider.of<PostViewModel>(context);
+    return Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.white,
+            elevation: 0.0,
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back, color: Colors.blue[600]),
+              onPressed: () {
+                router.pop(context);
+              },
+            ),
+            actions: [
+              IconButton(
+                  onPressed: isPost == true
+                      ? () async {
+                          var snapshot =
+                              await userProvider.getStaffById(user!.uid);
+                          postProvider
+                              .addPosts(Posts(
+                                  post: _postField.text,
+                                  posterId: user!.uid,
+                                  username: snapshot.username,
+                                  imageUrl: snapshot.imageUrl,
+                                  timeStamp: DateTime.now(),
+                                  isImportant: widget.isImportant))
+                              .then((value) {
+                            router.pop(context);
+                          });
+                        }
+                      : null,
+                  icon: Icon(Icons.post_add, color: Colors.blue[600]))
+            ],
+            title: Text("Add General Post",
+                style: TextStyle(
+                    color: Colors.blue[600],
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500))),
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.all(20),
+          color: Colors.white,
+          width: MediaQuery.of(context).size.width,
+          child: TextFormField(
+            controller: _postField,
+            minLines: 100,
+            maxLines: 250,
+            maxLength: 750,
+            onChanged: (text) {
+              if (text.length < 3) {
+                setState(() {
+                  isPost = false;
+                });
+              } else {
+                setState(() {
+                  isPost = true;
+                });
+              }
+            },
+            autofillHints: const [
+              "How are we doing today",
+              "whats going on at the office today"
+            ],
+            decoration: InputDecoration(hintText: "What is going on?"),
+          ),
+        ));
   }
-
 }
--- lib/ui/views/postview.dart
+++ lib/ui/views/postview.dart
@@ -6,110 +6,100 @@ import 'package:spacepal/core/viewmodels/post_view_model.dart';
 import 'package:spacepal/utils/date_time.dart';
 import 'package:readmore/readmore.dart';
 
-class PostView extends StatefulWidget{
+class PostView extends StatefulWidget {
   @override
-  PostViewState createState()=> PostViewState();
+  PostViewState createState() => PostViewState();
 }
-class PostViewState extends State<PostView>{
+
+class PostViewState extends State<PostView> {
   List<Posts> posts = [];
   TimeFormat timeFormat = TimeFormat();
   @override
   Widget build(BuildContext context) {
     final postProvider = Provider.of<PostViewModel>(context);
-   return Scaffold(
-     appBar: AppBar(
-       backgroundColor:Colors.grey[200],
-       title:Text(
-         "Spacepal",
-         style:TextStyle(
-           color:Colors.blue[600],
-           fontSize: 22,
-           fontWeight:FontWeight.w600,
-
-         )
-       ),
-       centerTitle: false,
-       actions: [
-          Icon(Icons.notifications, color:Colors.blue[600]),
-
-         
-       ],
-       elevation: 0.0,
-     ),
-     body:Container(
-       height: double.infinity,
-       color:Colors.grey[200],
-       child:StreamBuilder(
-         stream:postProvider.streamPosts(),
-         builder:(context, AsyncSnapshot<QuerySnapshot> snapshot){
-           if(snapshot.hasData){
-             posts = snapshot.data!.docs
-                  .map((doc) => Posts.fromMap(doc.data() as Map, doc.id)).toList();
-                  if(posts.length > 0){
-                   return ListView.builder(
-                     itemCount: posts.length,
-                     itemBuilder:(context, index)
-                       {
-                       return Container(
-                         margin:const EdgeInsets.only(top:15, left:7, right:7),
-                         decoration:BoxDecoration(
-                           borderRadius: BorderRadius.circular(10),
-                           color:Colors.white
-                         ),
-                         child: Column(
-                           children:[
-                                  ListTile(
-                                   leading:CircleAvatar(
-                                     radius: 25,
-                                     backgroundColor:Colors.blue,
-                                     backgroundImage:NetworkImage(posts[index].imageUrl)
-                                   //  backgroundImage: NetworkImage(posts[index].imageUrl),
-                                   ),
-                                   title:Text(posts[index].username,
-                                   style:TextStyle(
-                                     fontWeight: FontWeight.w600
-                                   )),
-                                   subtitle:Text("${timeFormat.displayDate(posts[index].timeStamp)} at ${timeFormat.displayTime(posts[index].timeStamp)}")),
-                                   Divider(),
-                                   Container(
-                                     height:5
-                                   ),
-                                 Container(
-                                      margin:EdgeInsets.only(left:5, right:5, bottom:6),
-                                   child: ReadMoreText(
-                                     posts[index].post,
-                                     trimLines: 5,
-                                     colorClickableText:Colors.blue[600],
-                                     trimMode: TrimMode.Line,
-                                     trimCollapsedText: 'read more',
-                                     trimExpandedText:'show less',
-                                     
-                                     style:TextStyle(
-                                       color:Colors.black
-                                     ) ,
-                                    
-                                   ))
-                         
-                           ]
-                         ),
-                     );}
-                     
-                   );
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.grey[200],
+          title: Text("spacepal",
+              style: TextStyle(
+                color: Colors.blue[600],
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+              )),
+          centerTitle: false,
+          actions: [
+            Icon(Icons.notifications, color: Colors.blue[600]),
+          ],
+          elevation: 0.0,
+        ),
+        body: Container(
+            height: double.infinity,
+            color: Colors.grey[200],
+            child: StreamBuilder(
+                stream: postProvider.streamPosts(),
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasData) {
+                    posts = snapshot.data!.docs
+                        .map((doc) => Posts.fromMap(doc.data() as Map, doc.id))
+                        .toList();
+                    if (posts.length > 0) {
+                      return ListView.builder(
+                          itemCount: posts.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              margin: const EdgeInsets.only(
+                                  top: 15, left: 10, right: 10),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.white),
+                              child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    ListTile(
+                                        leading: CircleAvatar(
+                                            radius: 25,
+                                            backgroundColor: Colors.blue,
+                                            backgroundImage: NetworkImage(
+                                                posts[index].imageUrl)
+                                            //  backgroundImage: NetworkImage(posts[index].imageUrl),
+                                            ),
+                                        title: Text(posts[index].username,
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.w600)),
+                                        subtitle: Text(
+                                            "${timeFormat.displayDate(posts[index].timeStamp)} at ${timeFormat.displayTime(posts[index].timeStamp)}")),
+                                    Container(height: 3),
+                                    Container(
+                                        margin: EdgeInsets.only(
+                                            left: 15, right: 15, bottom: 10),
+                                        child: ReadMoreText(
+                                          posts[index].post,
+                                          trimLines: 5,
+                                          colorClickableText: Colors.blue[600],
+                                          trimMode: TrimMode.Line,
+                                          trimCollapsedText: 'read more',
+                                          trimExpandedText: 'show less',
+                                          textAlign: TextAlign.left,
+                                          style: TextStyle(color: Colors.black),
+                                        ))
+                                  ]),
+                            );
+                          });
+                    } else {
+                      return Center(
+                          child: Text("No Posts Yet...",
+                              style: TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blue[600])));
+                    }
                   }
-                 else{   return Center(child:Text("No Posts Yet...",
-                    style:TextStyle(
-                      fontSize:25,
-                      fontWeight:FontWeight.bold,
-                      color:Colors.blue[600]
-                    )));
-                  
-                 }
-           }
-           return Center(child: CircularProgressIndicator(color: Colors.green[600],),);
-         }
-         )
-     )
-   );
+                  return Center(
+                    child: CircularProgressIndicator(
+                      color: Colors.green[600],
+                    ),
+                  );
+                })));
   }
-
 }
