04009e49b9ca86e666e5f5d51d0d775dab6f841f
Chukwudi Wisdom <55758868+override-bot@users.noreply.github.com>
2022-03-25 10:54:49 +0100

    update

--- lib/core/viewmodels/user_view_model.dart
+++ lib/core/viewmodels/user_view_model.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:spacepal/core/models/staff_model.dart';
 import 'package:spacepal/core/services/userapi.dart';
-import 'package:spacepal/locator.dart';
+
 class UserViewModel extends ChangeNotifier {
-   UserApi userApi = UserApi();
-   List<Staff> staff = [];
-    Future<List<Staff>> getAllStaff() async{
-      var result = await userApi.getDocuments();
-      staff = result.docs
-              .map((doc) => Staff.fromMap(doc.data() as Map<String, dynamic>, doc.id)).toList();
-      return staff;
+  UserApi userApi = UserApi();
+  List<Staff> staff = [];
+  Future<List<Staff>> getAllStaff() async {
+    var result = await userApi.getDocuments();
+    staff = result.docs
+        .map((doc) => Staff.fromMap(doc.data() as Map<String, dynamic>, doc.id))
+        .toList();
+    return staff;
   }
- 
-  Future<Staff> getStaffById(String id) async{
+
+  Future<Staff> getStaffById(String id) async {
     var doc = await userApi.getDocumentById(id);
     return Staff.fromMap(doc.data() as Map<String, dynamic>, doc.id);
   }
-  makeAdmin(userId){
+
+  makeAdmin(userId) {
     Map<String, dynamic> data = {
-          "isAdmin" : true,
+      "isAdmin": true,
     };
     return userApi.updateData(data, userId);
-      }
-
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,43 +4,40 @@ import 'package:provider/provider.dart';
 import 'package:spacepal/core/viewmodels/user_view_model.dart';
 
 import 'core/viewmodels/post_view_model.dart';
-import 'locator.dart';
 import 'ui/views/introscreen.dart';
 
-void main()async {
+void main() async {
 //  setupLocator();
-WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp();
-  runApp( MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-   MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
-   final PostViewModel postViewModel = PostViewModel();
-   final UserViewModel userViewModel = UserViewModel();
+  final PostViewModel postViewModel = PostViewModel();
+  final UserViewModel userViewModel = UserViewModel();
   @override
   Widget build(BuildContext context) {
-   return MultiProvider(providers: 
-        [
-            ChangeNotifierProvider(create: (_){
-              return postViewModel;
-            }),
-              ChangeNotifierProvider(create: (_){
-              return userViewModel;
-            })
-        ],
-        child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title:'spacepal',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-            
-          ),
-          home:  IntroScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) {
+          return postViewModel;
+        }),
+        ChangeNotifierProvider(create: (_) {
+          return userViewModel;
+        })
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'spacepal',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-   );
+        home: IntroScreen(),
+      ),
+    );
   }
 }
-
